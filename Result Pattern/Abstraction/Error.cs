f81a96e97b4ff3e73733cfeb53a948c977f4b708
omar <omarfaroq2003@gmail.com>
2025-03-07 21:22:58 +0200

    Add project files.

---
+++ Result Pattern/Abstraction/Error.cs
@@ -0,0 +1,6 @@
+﻿namespace SurveyBasket.Abstraction;
+
+public record Error(string Code , string Description)
+{ 
+    public static Error non => new (string.Empty,string.Empty);
+}
---
+++ Result Pattern/Services/Auth/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿
+using SurveyBasket.Abstraction;
+
+namespace SurveyBasket.Services.Auth;
+
+public interface IAuthService 
+{
+    Task<Result<AuthResponse>> SingInAsync(AuthRequest request);
+    Task<Result<AuthResponse>> GetRefreshTokenAsync(string Token,string RefreshToken);
+    Task<Result> RevokeRefreshTokenAsync(string Token,string RefreshToken);
+}
