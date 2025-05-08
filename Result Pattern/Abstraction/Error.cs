8439d19bf833659cc689b96ccd076a4878e1531c
omar abdo <omarfaroq2003@gmail.com>
2025-05-09 00:41:12 +0300

    make result pattern clear to do

--- Result Pattern/Abstraction/Error.cs
+++ Result Pattern/Abstraction/Error.cs
@@ -1,6 +1,6 @@
-﻿namespace SurveyBasket.Abstraction;
+﻿namespace Result_Pattern.Abstraction;
 
-public record Error(string Code , string Description)
-{ 
-    public static Error non => new (string.Empty,string.Empty);
+public record Error(string Code, string Description, int? StatusCode)
+{
+    public static Error Non => new(string.Empty, string.Empty, null);
 }
---
+++ Result Pattern/Abstraction/Errors/PollsErrors.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Result_Pattern.Abstraction.Errors;
+
+public static class PollsErrors
+{
+    public static readonly Error InvalidCredentials = new("Invalid credentials", "Invalid Poll Credentials", StatusCodes.Status404NotFound);
+    public static readonly Error NotFound = new("Invalid credentials", "NotFound", StatusCodes.Status404NotFound);
+    public static readonly Error DaplicatedTitle = new("Invalid credentials", "Daplicated Poll Title", StatusCodes.Status409Conflict);
+
+}
