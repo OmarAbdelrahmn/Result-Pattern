c7a12051db25631cabfe657827df265bef8997cc
omar abdo <omarfaroq2003@gmail.com>
2025-05-09 01:01:11 +0300

    Add New BaseResponse

---
+++ BaseResponse/BaseResponse.cs
@@ -0,0 +1,39 @@
+﻿using System.Net;
+
+namespace BaseResponse;
+
+public class BaseResponse<T>
+{
+    #region  Constructor
+    public BaseResponse()
+    {
+
+    }
+    public BaseResponse(T data, string? message = null)
+    {
+        Succeeded = true;
+        Message = message;
+        Data = data;
+    }
+    public BaseResponse(string message)
+    {
+        Succeeded = false;
+        Message = message;
+    }
+    public BaseResponse(string message, bool succeeded)
+    {
+        Succeeded = succeeded;
+        Message = message;
+    }
+    #endregion
+
+
+    #region Methods
+    public HttpStatusCode? StatusCode { get; set; }
+    public object? Meta { get; set; }
+    public bool? Succeeded { get; set; }
+    public string? Message { get; set; }
+    public List<string>? Errors { get; set; }
+    public T? Data { get; set; }
+    #endregion
+}
